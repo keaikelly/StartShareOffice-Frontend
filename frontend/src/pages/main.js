9652c890cd06b769aa5de283bf51b14e15dc244c
keaikelly <keaikelly@naver.com>
2025-02-21 10:47:20 +0900

    [Feat] 1-1 page 완성

--- frontend/src/pages/main.js
+++ frontend/src/pages/main.js
@@ -1,8 +1,26 @@
 import React from "react";
 import styles from "./main.module.css";
+import { BsChevronCompactDown } from "react-icons/bs";
 
 const Main = () => {
-  return <div className={styles.red}>여기는 메인페이지</div>;
+  return (
+    <div>
+      <div className={styles.content}>
+        <div className={styles.text}>
+          깨끗하고 편안한 <br />
+          공유 오피스 <br />
+        </div>
+        <div className={styles.tag}>
+          #가산디지털단지역 #독산역 #철산역 #광명역
+        </div>
+        <div className={styles.down}>
+          <BsChevronCompactDown />
+        </div>
+      </div>
+
+      {/* <img className={styles.img} src="./main.jpeg"></img> */}
+    </div>
+  );
 };
 
 export default Main;
